2637af5a5c677c140f8985bb5337409737365c31
Krishna Harsha Voora <krvoora@redhat.com>
2023-02-07 21:11:59 +0530

    Updates the run_me.sh
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- run_me.sh
+++ run_me.sh
@@ -4,31 +4,38 @@
 # Define common parameters
 ################################################
 
-cd /usr/share/bcc/tools/
+export PATH=$PATH:/usr/share/bcc/tools/
 TEMP_DIR=`mktemp -d`
 pushd $TEMP_DIR
 
-# List CPU details
+################################################ List CPU details
 lscpu > lscpu_details
 
-# Uname & Hostname details
+################################################ Uname & Hostname details
 uname -a > uname_details
-hostname -a > hostname_details
 
-# Cachestat
-./cachestat 5 20 > cachestat_details
+################################################ Cachestat
+cachestat 5 20 > cachestat_details
 
-# SoftIRQs
-./softirqs 5 20 > softirqs.intel_details
+################################################ SoftIRQs
+softirqs 5 20 > softirqs_details
 
-# LoadAVG
-cat /proc/loadavg > loadavg.intel_details
+################################################ LoadAVG
+cat /proc/loadavg > loadavg_details
 
-# 
-./profile 60 > /tmp/profile.intel_details
+# Defaults at 49 probes, which might be very less
+# For more probes 
+# profile -F 999  ## Run for duration of workload
+# Lesse
+# profile -F 255 ## Should always be above 255
+profile 60 > profile_details
 
-# System wide perf stats
+################################################ System wide perf stats
 perf stat -a -- sleep 10 > perf_stat_details
 
-
-echo "Default details captured"
+################################################ Give details of Tarball
+echo "Details captured"
+popd
+echo "Create Tarball"
+tar -cvf $TEMP_DIR.tar $TEMP_DIR/
+echo "Unpack Tarball $TEMP_DIR.tar"
